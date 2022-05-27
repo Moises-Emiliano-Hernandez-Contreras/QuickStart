f027ac3cca7a05e03a58d3e522089b45aff0074e
Moises-Emiliano-Hernandez-Contreras <73419469+Moises-Emiliano-Hernandez-Contreras@users.noreply.github.com>
2022-05-26 22:18:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -131,8 +131,12 @@ items.addEventListener('click',e=>{
 
 const datosFetch= async () => {
       try {
-            const res = await fetch('https://www.freetogame.com/api/games?platform=pc&category=shooter', { 
-                  mode: 'no-cors' // 'cors' by default
+            const res = await fetch('https://cors-anywhere.herokuapp.com/'+'https://www.freetogame.com/api/games?platform=pc&category=shooter', { 
+                  method: 'GET',
+                  headers: new Headers({
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                  }),
             })
             const data = await res.json()            
             pintar_tarjeta(data)
